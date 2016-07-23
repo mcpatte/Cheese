082bdc70240735c66c229b8d8bdde8029963f9f4
llo1 <louislo@gmail.com>
2016-07-23 14:23:47 -0700

    integrate redux into react

--- public/actions/index.js
+++ public/actions/index.js
@@ -1,21 +1,21 @@
+import axios from 'axios'
 
 export const REQUEST_TASKS = 'REQUEST_TASKS'
-export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const RECEIVE_TASKS = 'RECIEVE_TASKS'
 export const POST_TASK = 'POST_TASK'
 
-export function requestTasks(userID) {
-  return dispatch => {
-    dispatch(requestTasks())
-    return fetch('/tasks')
-      .then(response => response.json())
-      .then(json => dispatch(receiveTasks(json)))
+
+export function requestTasks() {
+  return {
+    type: REQUEST_TASKS
   }
 }
 
-export function receiveTasks(json) {
+export function receiveTasks(tasks) {
   return {
-    type: RECEIVE_POSTS,
-    data: json.data.children.map(child => child.data),
+    type: RECEIVE_TASKS,
+    tasks,
+    data: json.data.map(task => task),
     receivedAt: Date.now()
   }
 }
@@ -26,12 +26,19 @@ export function postTask() {
   }
 }
 
+export function fetchTasks() {
+  return dispatch => {
+    console.log('fetchTasks')
+    return axios.get('http://localhost:3000/tasks')
+      .then(response => {
+        console.log(response)
+        response.data.json()})
+      .then(json => dispatch(receiveTasks(json)))
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+}
 
 
 
-//requestTasks
-
-
-
-//receiveTasks
-//postTask
