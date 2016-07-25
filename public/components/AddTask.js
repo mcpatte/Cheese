6f66f540f35d200dbbed842c06cfefb16a3e30ca
Chase Patterson <mcpatte@gmail.com>
2016-07-24 17:31:03 -0700

    (feat) add add task button to the navBar

--- public/components/AddTask.js
+++ public/components/AddTask.js
@@ -1,29 +1,67 @@
-import React from 'react'
-import {Fieldset, Field, createValue} from 'react-forms'
+// import React from 'react'
+// import { Field, reduxForm } from 'redux-form'
+// const  { DOM: { input, select, textarea } } = React
 
-class Form extends React.Component {
+// const SimpleForm = (props) => {
+//   const { handleSubmit, pristine, reset, submitting } = props
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label>First Name</label>
+//         <div>
+//           <Field name="firstName" component={input} type="text" placeholder="First Name"/>
+//         </div>
+//       </div>
+//       <div>
+//         <label>Last Name</label>
+//         <div>
+//           <Field name="lastName" component={input} type="text" placeholder="Last Name"/>
+//         </div>
+//       </div>
+//       <div>
+//         <label>Email</label>
+//         <div>
+//           <Field name="email" component={input} type="email" placeholder="Email"/>
+//         </div>
+//       </div>
+//       <div>
+//         <label>Sex</label>
+//         <div>
+//           <label><Field name="sex" component={input} type="radio" value="male"/> Male</label>
+//           <label><Field name="sex" component={input} type="radio" value="female"/> Female</label>
+//         </div>
+//       </div>
+//       <div>
+//         <label>Favorite Color</label>
+//         <div>
+//           <Field name="favoriteColor" component={select}>
+//             <option></option>
+//             <option value="ff0000">Red</option>
+//             <option value="00ff00">Green</option>
+//             <option value="0000ff">Blue</option>
+//           </Field>
+//         </div>
+//       </div>
+//       <div>
+//         <label htmlFor="employed">Employed</label>
+//         <div>
+//           <Field name="employed" id="employed" component={input} type="checkbox"/>
+//         </div>
+//       </div>
+//       <div>
+//         <label>Notes</label>
+//         <div>
+//           <Field name="notes" component={textarea}/>
+//         </div>
+//       </div>
+//       <div>
+//         <button type="submit" disabled={pristine || submitting}>Submit</button>
+//         <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+//       </div>
+//     </form>
+//   )
+// }
 
-  constructor(props) {
-    super(props)
-    let formValue = createValue({
-      value: props.value,
-      onChange: this.onChange.bind(this)
-    })
-    this.state = {formValue}
-  }
-
-  onChange(formValue) {
-    this.setState({formValue})
-  }
-
-  render() {
-    return (
-      <Fieldset formValue={this.state.formValue}>
-        <Field select="taskName" label="First name" />
-        <Field select="lastName" label="Last name" />
-      </Fieldset>
-    )
-  }
-}
-
-// export default Form
+// export default reduxForm({
+//   form: 'simple'  // a unique identifier for this form
+// })(SimpleForm)
--- public/components/Navbar.js
+++ public/components/Navbar.js
@@ -8,18 +8,116 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Avatar from 'material-ui/Avatar'
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Field, reduxForm } from 'redux-form';
+import {TextField} from 'redux-form-material-ui';
+import DatePicker from 'material-ui/DatePicker';
+import DropDownMenu from 'material-ui/DropDownMenu';
+
+
 injectTapEventPlugin();
 
+const styles = {
+  width: '40%',
+  maxWidth: 'none',
+};
+
+const textStyles = {
+
+};
 
 
 class Nav extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false, value: 1};
+  };
+
   render(){
+  const handleChange = (event, index, value) => this.setState({value});
+
+  const GetDate = () => (
+    <div>
+      <DatePicker hintText="Due date" mode="landscape" />
+    </div>
+  );
+
+  const GetText = () => (
+    <div>
+      <TextField hintText="Task Name"/><br />
+    </div>
+  )
+  const handleSubmit = () => {
+    console.log("Hey you submitted the form!");
+    };
+
+  const handleOpen = () => {
+    console.log("hey")
+    this.setState({open: true});
+  };
+
+  const handleClose = () => {
+    console.log("close")
+    this.setState({open: false});
+  };
+       const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={handleClose}
+      />,
+    ];
+
+
     return (
   <AppBar
     title="Cheese"
     className="navBar"
     style={{position: 'fixed'}}
-    iconElementRight={<Avatar src="https://pixabay.com/static/uploads/photo/2016/03/31/19/57/avatar-1295406_960_720.png"/>}
+    iconElementRight={
+      <div>
+      <RaisedButton label="Add new task" onTouchTap={handleOpen} />
+      <Dialog
+          title="Add new task!"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+          contentStyle={styles}
+        >
+          <form className= 'form'>
+            Task Name:<br/>
+            <TextField hintText="Task name"/><br />
+            Due date:<br/>
+            <DatePicker hintText="Due date" mode="landscape" />
+            Points:<br/>
+            <TextField hintText="Points"/><br />
+            Category:<br/>
+              <div>
+                <DropDownMenu
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    style={styles.customWidth}
+                    autoWidth={false}
+                    >
+                  <MenuItem value={1} primaryText="To-Do" />
+                  <MenuItem value={2} primaryText="In progress" />
+                  <MenuItem value={3} primaryText="Done" />
+                </DropDownMenu>
+            </div>
+          </form>
+        </Dialog>
+      </div>
+    }
     iconElementLeft={
       <IconMenu
         iconButtonElement={
@@ -36,6 +134,9 @@ class Nav extends React.Component {
   />
   )}
 }
+
+
+
 
 
 export default Nav
